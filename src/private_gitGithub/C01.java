dbf9693eda01f5ac11170ba108328ab6d913b021
Emre Yilgen <emreyilgen@gmail.com>
2023-11-04 23:46:50 +0300

    first change

--- src/private_gitGithub/C01.java
+++ src/private_gitGithub/C01.java
@@ -3,5 +3,6 @@ package private_gitGithub;
 public class C01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("that's it");
     }
 }
